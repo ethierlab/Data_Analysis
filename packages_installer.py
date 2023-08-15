2cc77f5972ae5ffb789b12abf81f20eb1c074efc
visav33 <58119437+visav33@users.noreply.github.com>
2023-08-15 12:47:13 -0400

    Update packages_installer.py

--- packages_installer.py
+++ packages_installer.py
@@ -5,7 +5,7 @@ def install(package):
     for package in packages:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
             
-packages = ["numpy", "scipy", "pandas", "ipywidgets", "IPython", "matplotlib", "jupyter"]
+packages = ["numpy", "scipy", "pandas", "ipywidgets", "IPython", "matplotlib", "jupyter", "h5py"]
 
 if __name__ == '__main__':
     install(packages)
